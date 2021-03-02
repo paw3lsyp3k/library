e2ada69083622625157b3fa82422a4f8871a01e6
Paweł <paw3lsyp3k@gmail.com>
2021-03-02 01:32:54 +0100

    all books and readers

---
+++ all_books.php
@@ -0,0 +1,42 @@
+<?php
+    include_once 'php/dbh.inc.php'
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/add.css">
+    <title>Library - Spis książek</title>
+</head>
+<body>
+    <div class="topbar">
+        <a href="index.html">
+            <i class="fas fa-arrow-circle-left"></i>
+            Powrót
+        </a>
+    </div>
+    <div class="header">
+        Spis książek
+    </div>
+    <?php
+    $sql = "SELECT * FROM ksiazki;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if($resultCheck > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            echo $row['ID'] . " ";
+            echo $row['tytul'] . " ";
+            echo $row['autor'] . " ";
+            echo $row['rok_wydania'] . "<br>";
+        }
+    }
+
+    ?>
+</body>
+</html>
