e7c0bffe04374c1f72721d7f6924f118900ab1f9
Paweł <paw3lsyp3k@gmail.com>
2021-03-02 20:12:06 +0100

    final v

---
+++ all_readers.php
@@ -0,0 +1,46 @@
+<?php
+    include_once 'php/dbh.inc.php'
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/add.css">
+    <title>Library - Spis czytelników</title>
+</head>
+<body>
+    <div class="topbar">
+        <a href="index.html">
+            <i class="fas fa-arrow-circle-left"></i>
+            Powrót
+        </a>
+    </div>
+    <div class="header">
+        Spis czytelników
+    </div>
+    <?php
+    $sql = "SELECT * FROM czytelnicy;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if($resultCheck > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            echo $row['imie'] . " ";
+            echo $row['nazwisko'] . "<br>";
+            echo $row['telefon'] . "<br>";
+            echo $row['mail'] . "<br>";
+            echo $row['ulica'] . " ";
+            echo $row['nr_domu'] . "<br>";
+            echo $row['miejscowosc'] . " ";
+            echo $row['kod'] . "<br>" . "<br>";
+        }
+    }
+
+    ?>
+</body>
+</html>
